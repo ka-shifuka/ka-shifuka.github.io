14706dd47b5556227ab194a1f0b22ca784ac891c
ka-shifuka <aryadewanggapuja@gmail.com>
2025-05-03 08:57:20 +0700

    Testing (absolute URL path)

---
+++ Todo.md
@@ -0,0 +1,1 @@
+Make sure all image is compressed
---
+++ docs/assets/about-QoQqbWMb.js
@@ -0,0 +1,1 @@
+import{c as a,o as t,b as e,N as o,F as r,e as c,f as n}from"./Footer-CEOno448.js";const p={__name:"App",setup(s){return(_,m)=>(t(),a(c,null,[e(o),e(r)],64))}};n(p).mount("#app");
